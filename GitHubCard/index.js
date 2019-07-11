1cb56dfc72d476ecc2a660b916bd07424b0e695d
Dora Belme <contact.dorabelme@gmail.com>
2019-07-11 12:37:13 -0700

    added get requests

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,17 +1,23 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
 
+const mainCard = document.querySelector(".cards");
 axios.get("https://api.github.com/users/dorabelme")
   .then(data => {
-    // Handles success: here is wgere we get the results from server
+    // Handles success: here is where we get the results from server
     console.log("response", data)
+    const userObj = data.data
+    const element = createCard(userObj);
+    mainCard.appendChild(element);
   })
   .catch(error => {
     // Handles failure
-    console.log("ERROR", err)
+    console.log("The Github API is currently down, try again later", error)
   });
+
+
+/* Step 1: using axios, send a GET request to the following URL 
+  (replacing the palceholder with your Github name):
+  https://api.github.com/users/<your name>
+*/
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -23,6 +29,10 @@ axios.get("https://api.github.com/users/dorabelme")
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+// the main DOM node to attach our dynamic content to
+// const mainCard = document.querySelector('.cards')
+
+
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -34,7 +44,22 @@ axios.get("https://api.github.com/users/dorabelme")
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+followersArray.forEach(follower => {
+  axios.get(`https://api.github.com/users/${follower}`)
+    .then(data => {
+      // Handles Success: here's where we get the results from server
+      console.log("response", data);
+      const userObj = data.data;
+      const element = createCard(userObj);
+      mainCard.appendChild(element);
+    })
+    .catch(error => {
+      // Handles failure:
+      console.log("The dogs API is currently down, try again later", error);
+    });
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -55,10 +80,8 @@ const followersArray = [];
 </div>
 
 */
-const cards = document.querySelector('.cards');
 
-cards.appendStyle
-function createCard({}) {
+function createCard(userObj) {
   // create the elements
   const card = document.createElement('div');
   const img = document.createElement("img");
@@ -80,23 +103,23 @@ function createCard({}) {
   
 
   // set the content
-  img.src = `${imageUrl}`;
-  name.textContent = `${nameUser}`;
-  location.textContent = `Location: ${locationUser}`;
-  address.href = `${addressUrl}`;
-  address.textContent = `${addressUrl}`;
-  followers.textContent = `Followers: ${followerCount}`;
-  following.textContent = `Following: ${followingCount}`;
-  bio.textContent = `Bio: ${bioUser}`;
+  img.src = userObj.avatar_url;
+  name.textContent = userObj.name;
+  location.textContent = `Location: ${userObj.location}`;
+  address.href = `${userObj.html_url}`;
+  address.textContent = `${userObj.html_url}`;
+  followers.textContent = `Followers: ${userObj.followers}`;
+  following.textContent = `Following: ${userObj.following}`;
+  bio.textContent = `Bio: ${userObj.bio}`;
 
 
   // put together structure
-  card.appendChild(img, cardInfo)
-  cardInfo.appendChild(name, userName, location, profile, followers, following, bio)
-  profile.appendChild(address)
+  card.appendChild(img, cardInfo);
+  cardInfo.appendChild(name, userName, location, profile, followers, following, bio);
+  profile.appendChild(address);
 
-  return card
-}
+  return card;
+};
 
 /* List of LS Instructors Github username's: 
   tetondan
